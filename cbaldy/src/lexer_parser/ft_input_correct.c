ad0df0fd8c7b4a97b5f35a7c4f456ac53b665c82
Thomas SANZEY <tsanzey@e2r3p7.42.fr>
2016-03-29 15:35:27 +0200

    gestion d'erreur sur filename

--- cbaldy/src/lexer_parser/ft_input_correct.c
+++ cbaldy/src/lexer_parser/ft_input_correct.c
@@ -16,7 +16,7 @@ int		ft_command_isvalid(t_token *tok)
 {
 	printf("cmd-> %s, type %d\n", tok->content, tok->type);
 	if (tok->type == WORDS || tok->type == SEMICOL
-		|| tok->type == AMPERSAND)
+		|| tok->type == AMPERSAND || tok->type == FILENAME)
 	{
 		if (tok->next)
 			return (ft_command_isvalid(tok->next));
