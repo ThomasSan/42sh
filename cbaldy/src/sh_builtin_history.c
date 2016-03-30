37257965504d0e1edd71b666033697e4f33d48b5
Damien BALDY <dbaldy@e3r8p16.42.fr>
2016-03-30 12:23:58 +0200

    no empty lines in hist

--- cbaldy/src/sh_builtin_history.c
+++ cbaldy/src/sh_builtin_history.c
@@ -6,11 +6,26 @@
 /*   By: dbaldy <dbaldy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/26 19:22:58 by dbaldy            #+#    #+#             */
-/*   Updated: 2016/03/30 10:54:29 by dbaldy           ###   ########.fr       */
+/*   Updated: 2016/03/30 12:23:01 by dbaldy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+static int		non_empty(t_com_list *begin)
+{
+	t_com_list	*buf;
+
+	buf = begin;
+	while (buf)
+	{
+		if (buf->c != ' ' && buf->c != '\0')
+			return (0);
+		else
+			buf = buf->next;
+	}
+	return (1);
+}
 
 t_hist_list		*retrieve_history(int flag, t_com_list *begin)
 {
@@ -28,7 +43,7 @@ t_hist_list		*retrieve_history(int flag, t_com_list *begin)
 	}
 	else if (flag == 2)
 		return (copy_hist(hist));
-	else
+	else if (non_empty(begin) == 0)
 	{
 		hist_list_new(&hist);
 		hist->old = begin;
