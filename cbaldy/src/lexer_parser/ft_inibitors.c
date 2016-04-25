0ba797f8e4c73bd9e8dc92de9d462644fcc93e39
Thomas SANZEY <tsanzey@e2r8p8.42.fr>
2016-04-25 12:53:09 +0200

    Mise a la norme 2

---
+++ cbaldy/src/lexer_parser/ft_inibitors.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_inibitors.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsanzey <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/25 12:43:18 by tsanzey           #+#    #+#             */
+/*   Updated: 2016/04/25 12:52:01 by tsanzey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+t_token		*join_tokens(t_token *tok)
+{
+	t_token *tmp;
+	char	*str;
+
+	tmp = tok;
+	while (tok)
+	{
+		if (tok->next && tok->type == WORDS && tok->next->type == WORDS)
+		{
+			str = tok->content;
+			tok->content = ft_strjoin(tok->content, tok->next->content);
+			free(str);
+			pop_middle_token(tok->next);
+		}
+		else
+			tok = tok->next;
+	}
+	return (tmp);
+}
+
+t_token		*inibitor_handler(t_token *tok)
+{
+	t_token *tmp;
+
+	tmp = tok;
+	while (tok)
+	{
+		if (tok->type == BACKSLASH)
+		{
+			if (check_next_token(tok) != WORDS)
+				tok->next->type = WORDS;
+			if (tmp == tok)
+				tmp = tmp->next;
+			tok = pop_middle_token(tok);
+		}
+		else
+			tok = tok->next;
+	}
+	return (tmp);
+}
