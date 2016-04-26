8335b8ba3de460442c2f52979510db20996b9c47
Christophe BALDY <cbaldy@e2r7p10.42.fr>
2016-04-26 11:19:06 +0200

    copy

--- cbaldy/src/yank_line.c
+++ cbaldy/src/yank_line.c
@@ -6,46 +6,50 @@
 /*   By: cbaldy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 10:06:21 by cbaldy            #+#    #+#             */
-/*   Updated: 2016/04/22 13:48:43 by cbaldy           ###   ########.fr       */
+/*   Updated: 2016/04/26 11:18:12 by cbaldy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static int	yank_end_line(t_line_list **first)
+static int	yank_end_line(t_line_list **first, char c)
 {
 	t_com_list	*tmp;
-	int			i;
+	int			i[3];
 
+	i[1] = (c == 21 ? 2 : 1);
 	tmp = (*first)->begin;
-	i = (*first)->marge;
-	while (tmp != NULL && i++ < g_local->curs - 1)
+	i[0] = (*first)->marge;
+	i[2] = g_local->curs;
+	while (tmp != NULL && i[0]++ < g_local->curs - 1)
 		tmp = tmp->next;
 	while (tmp != NULL && tmp->c != '\n')
 	{
-		tmp->op = 2;
+		tmp->op = i[1];
 		tmp = tmp->next;
 	}
 	copy_end_mode(first);
-	while (term_mv_horizontal(3, first, 1) == 0)
+	while (term_mv_horizontal(3, first, 1) == 0 && g_local->curs < i[2])
 		;
 	return (0);
 }
 
-static int	yank_begin_line(t_line_list **first)
+static int	yank_begin_line(t_line_list **first, char c)
 {
 	t_com_list	*tmp;
-	int			i;
+	int			i[3];
 
-	i = (*first)->marge;
+	i[1] = (c == 11 ? 2 : 1);
+	i[0] = (*first)->marge;
 	tmp = (*first)->begin;
-	while (tmp != NULL && i++ < g_local->curs - 1)
+	i[2] = g_local->curs;
+	while (tmp != NULL && i[0]++ < g_local->curs - 1)
 	{
-		tmp->op = 2;
+		tmp->op = i[1];
 		tmp = tmp->next;
 	}
 	copy_end_mode(first);
-	while (term_mv_horizontal(4, first, 1) == 0)
+	while (term_mv_horizontal(3, first, 1) == 0 && g_local->curs < i[2])
 		;
 	return (0);
 }
@@ -54,9 +58,9 @@ int			yank_line(char c, t_line_list **first)
 {
 	if (c == 25)
 		return (copy_paste(first));
-	if (c == 21)
-		return (yank_end_line(first));
-	else if (c == 11)
-		return (yank_begin_line(first));
+	if (c == 21 || c == 16)
+		return (yank_end_line(first, c));
+	else if (c == 11 || c == 8)
+		return (yank_begin_line(first, c));
 	return (0);
 }
