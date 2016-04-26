4b20ceabeadcdca650777ba5014e39d329dcd2e3
Christophe BALDY <cbaldy@e2r7p10.42.fr>
2016-04-26 14:46:01 +0200

    21sh cd ok

--- 21sh/src/globing_func.c
+++ 21sh/src/globing_func.c
@@ -6,25 +6,11 @@
 /*   By: dbaldy <dbaldy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 18:55:47 by dbaldy            #+#    #+#             */
-/*   Updated: 2016/04/25 13:59:07 by dbaldy           ###   ########.fr       */
+/*   Updated: 2016/04/26 11:52:12 by cbaldy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "glob.h"
-
-int					alias_ihome(char **path, int i)
-{
-	char	*result;
-	int		j;
-
-	if ((j = sh_is_new_var("HOME")) < 0)
-		return (0);
-	result = ft_strchr(g_env[j], '=') + 1;
-	result = ft_ireplace_str(*path, result, i, 1);
-	free(*path);
-	*path = result;
-	return (0);
-}
+#include "shell.h"
 
 int					alias_home(char **path)
 {
@@ -72,32 +58,4 @@ char				**tabl_compl(char *path)
 	}
 	closedir(dip);
 	return (res);
-}
-
-t_glob_list			*build_match_list(char **path, char *word)
-{
-	char			**poss;
-	int				i;
-	t_glob_list		*glob;
-
-	i = 0;
-	glob = NULL;
-	if ((poss = tabl_compl(*path)) == NULL)
-		return (NULL);
-	while (poss[i])
-	{
-		if (match(poss[i], word) == 1)
-		{
-			if ((poss[i][0] == '.' && word[0] != '.') || ft_strcmp(poss[i], ".")
-					== 0 || ft_strcmp(poss[i], "..") == 0)
-				;
-			else
-				glob = add_elem_glob(poss[i], glob);
-		}
-		i++;
-	}
-	while (glob && glob->prev)
-		glob = glob->prev;
-	ft_free_tab(poss);
-	return (glob);
 }
